b471850f24d9231d4bd5cd97c04caeea5f85ee9d
chain <klaus.mika1052@gmail.com>
2023-06-19 18:59:51 -0300

    completed exercise-05

--- modulo-01/exercicios-backend-variaveis/05/index.js
+++ modulo-01/exercicios-backend-variaveis/05/index.js
@@ -1,1 +1,17 @@
 
+function calcDistanceBetweenPoints(firstPoint, secondPoint) {
+  const { x1, y1 } = firstPoint
+  const { x2, y2 } = secondPoint
+
+  const firstPointCalc = (x2 - x1) ** 2;
+  const secondPointCalc = (y2 - y1) ** 2;
+  
+  const distance = Math.sqrt(firstPointCalc + secondPointCalc)
+
+  return distance
+}
+
+const firstPoint = {x1: 1, y1: 1 }
+const secondPoint = {x2: 1, y2: 4 }
+
+console.log(calcDistanceBetweenPoints(firstPoint, secondPoint))
