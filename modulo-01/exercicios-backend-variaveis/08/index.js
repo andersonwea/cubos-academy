048fefd18ebff9522c6f00341281a46edaae7cc5
chain <klaus.mika1052@gmail.com>
2023-06-19 20:01:16 -0300

    completed exercise-08

---
+++ modulo-01/exercicios-backend-variaveis/08/index.js
@@ -0,0 +1,12 @@
+function calcPerimeterAndCircleArea(circleRadius) {
+  const pi = Math.PI
+  
+  const circleLength = 2 * pi * circleRadius
+  const circleArea = pi * (circleRadius ** 2)
+
+  return {circleLength, circleArea}
+}
+
+const circleRadius = 1
+
+console.log(calcPerimeterAndCircleArea(circleRadius))
