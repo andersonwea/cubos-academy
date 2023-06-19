dfd6a2d6f61890184264c922cdd772b876de65ae
chain <klaus.mika1052@gmail.com>
2023-06-19 20:43:58 -0300

    completed exercise-12

---
+++ modulo-01/exercicios-backend-variaveis/12/index.js
@@ -0,0 +1,11 @@
+function calcInterestRate(totalPaid, capital, months) {
+  const interestRate = (totalPaid / capital) ** (1 / months) - 1 
+
+  return (interestRate * 100).toFixed(3)
+}
+
+const totalPaid = 90_000
+const capital = 60_000
+const months = 24
+
+console.log(calcInterestRate(totalPaid, capital, months))
