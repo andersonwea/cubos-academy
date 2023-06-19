ce2a71054896b43e55d88cb084da4f3a82e10948
chain <klaus.mika1052@gmail.com>
2023-06-19 20:48:13 -0300

    completed exercise-13

---
+++ modulo-01/exercicios-backend-variaveis/13/index.js
@@ -0,0 +1,10 @@
+function calcCylinderArea(radius, cylinderHeight) {
+  const totalCylinderArea = 2 * 3 * ( 3 + 1)
+  
+  return totalCylinderArea
+}
+
+const radius = 3
+const cylinderHeight = 1
+
+console.log(calcCylinderArea(radius, cylinderHeight))
