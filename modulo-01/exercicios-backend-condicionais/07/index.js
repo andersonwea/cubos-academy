cd6f53c139b76ec43aa72d236c50e7ad4ebe6875
chain <klaus.mika1052@gmail.com>
2023-06-20 18:36:02 -0300

    feat: completed exercise-07

--- modulo-01/exercicios-backend-condicionais/07/index.js
+++ modulo-01/exercicios-backend-condicionais/07/index.js
@@ -3,3 +3,14 @@ const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
 //seu código aqui
+function checkIncomeTax(aposentada, portadoraDeDoenca, totalDeRendimentos) {
+  if(aposentada || portadoraDeDoenca) {
+    return 'ISENTA'
+  } else if(totalDeRendimentos > 28_559.70) {
+    return 'PEGA LEÃO'
+  } else {
+    return 'VAZA LEÃO! JÁ TA DÍFICIL SEM VOCÊ'
+  }
+}
+
+console.log(checkIncomeTax(aposentada, portadoraDeDoenca, totalDeRendimentos))
