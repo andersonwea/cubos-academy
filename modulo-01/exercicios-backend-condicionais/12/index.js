613b1c1ce742dd697cd978d4fa2156250206da42
chain <klaus.mika1052@gmail.com>
2023-06-20 20:44:57 -0300

    feat: completed exercise-12

--- modulo-01/exercicios-backend-condicionais/12/index.js
+++ modulo-01/exercicios-backend-condicionais/12/index.js
@@ -1,1 +1,25 @@
 const diaDaSemana = 3;
+
+function checkWeekDay(diaDaSemana) {
+  const days = {
+    0: 'Domingo',
+    1: 'Segunda-Feira',
+    2: 'Terça-Feira',
+    3: 'Quarta-Feira',
+    4: 'Quinta-Feira',
+    5: 'Sexta-Feira',
+    6: 'Sábado'
+  }
+
+  if(diaDaSemana > 6 || diaDaSemana < 0) {
+    return 'O dia da semana informado não é válido'
+  }
+
+  for(let day in days) {
+    if(Number(day) === diaDaSemana) {
+      return days[day]
+    }
+  }
+}
+
+console.log(checkWeekDay(diaDaSemana));
