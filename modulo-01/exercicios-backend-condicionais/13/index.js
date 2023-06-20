c0ad96362a4773d9b28495ab06e42afb6d88d340
chain <klaus.mika1052@gmail.com>
2023-06-20 20:52:47 -0300

    feat: completed exercise-13

--- modulo-01/exercicios-backend-condicionais/13/index.js
+++ modulo-01/exercicios-backend-condicionais/13/index.js
@@ -4,3 +4,23 @@ const tipoDePagamento = "credito";
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+function takeDiscount(tipoDePagamento, valorDoProduto) {
+  let totalToPay = 0
+  
+  const discounts = {
+    'cheque': 0.03,
+    'credito': 0.05,
+    'dinheiro': 0.1,
+    'debito': 0.1
+  }
+
+  for(let paymentMethod in discounts) {
+    if(tipoDePagamento === paymentMethod) {
+      totalToPay = valorDoProduto - (valorDoProduto * discounts[paymentMethod])
+    }
+  }
+
+  return totalToPay / 100
+}
+
+console.log(takeDiscount(tipoDePagamento, valorDoProduto))
