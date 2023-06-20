ee75af0e514fb5611647ff774d22bc2535abf68e
chain <klaus.mika1052@gmail.com>
2023-06-20 17:05:41 -0300

    feat: completed exercise-01

---
+++ modulo-01/exercicios-backend-condicionais/01/index.js
@@ -0,0 +1,21 @@
+const jogada1 = "pedra"
+const jogada2 = "tesoura"
+
+//seu código aqui
+function checkWinner(jogada1, jogada2) {
+  const moves = [jogada1, jogada2]
+
+  if(jogada1 === jogada2) {
+    return 'empate'
+  }
+
+  if(moves.includes('pedra') && moves.includes('tesoura')) {
+    return 'pedra'
+  } else if(moves.includes('pedra') && moves.includes('papel')) {
+    return 'papel'
+  } else if(moves.includes('tesoura') && moves.includes('papel')) {
+    return 'teoura'
+  }
+}
+
+console.log(checkWinner(jogada1, jogada2))
