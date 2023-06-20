e331a041a30bb2656a5016beaa53689e0106ae03
chain <klaus.mika1052@gmail.com>
2023-06-20 17:10:38 -0300

    feat: completed exercise-02

--- modulo-01/exercicios-backend-condicionais/02/index.js
+++ modulo-01/exercicios-backend-condicionais/02/index.js
@@ -2,3 +2,14 @@ const jogada1 = 5;
 const jogada2 = 3;
 
 //seu código aqui
+function checkEvenOrOdd(jogada1, jogada2) {
+  const total = jogada1 + jogada2
+
+  if(total % 2 == 0) {
+    return 'par'
+  } else {
+    return 'impar'
+  }
+}
+
+console.log(checkEvenOrOdd(jogada1, jogada2))
