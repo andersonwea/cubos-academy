2f1a0c11294d268dab9a416b84c7f49b29d59d35
chain <klaus.mika1052@gmail.com>
2023-06-20 19:25:41 -0300

    feat: completed exercise-09

--- modulo-01/exercicios-backend-condicionais/09/index.js
+++ modulo-01/exercicios-backend-condicionais/09/index.js
@@ -1,1 +1,19 @@
-const nota = 8.5;
+const nota = 8.9;
+
+const notes = {
+  4: 'E',
+  6: 'D',
+  8: 'C',
+  9: 'B',
+  10: 'A'
+}
+
+function transformNoteToConcept(nota) {
+  for(let note in notes) {
+    if(nota < note) {
+      return notes[note]
+    }
+  }
+}
+
+console.log(transformNoteToConcept(nota))
