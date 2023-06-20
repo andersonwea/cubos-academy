72096f4cb6c8736475d224deb543ba17d23ed1ca
chain <klaus.mika1052@gmail.com>
2023-06-20 18:03:12 -0300

    feat: completed exercise-04

---
+++ modulo-01/exercicios-backend-condicionais/04/index.js
@@ -0,0 +1,32 @@
+const ladoA = 3;
+const ladoB = 3;
+
+const doubleName = {
+  0: 'Branco',
+  1: 'Ás',
+  2: 'Duque',
+  3: 'Terno',
+  4: 'Quadra',
+  5: 'Quina',
+  6: 'Sena'
+}
+
+//seu código aqui
+function isDouble(ladoA, ladoB) {
+  return ladoA === ladoB
+}
+
+function checkDoubleName(ladoA, ladoB) {
+  const isDoubleResult = isDouble(ladoA, ladoB)
+
+  if(!isDoubleResult) return 'Não é BUCHA'
+
+  for(let double in doubleName) {
+    if(ladoA === +double) {
+      return doubleName[double]
+    }
+  }
+}
+
+console.log(checkDoubleName(ladoA, ladoB))
+
