006b65bcbb29acfaa25f22b3fd2c2394caf939b0
chain <klaus.mika1052@gmail.com>
2023-06-20 20:16:29 -0300

    completed exercise-10

--- modulo-01/exercicios-backend-condicionais/10/index.js
+++ modulo-01/exercicios-backend-condicionais/10/index.js
@@ -1,1 +1,20 @@
 const caractere = "E";
+
+function indentifyCaractere(caractere) {
+  const regexs = [
+    {message: 'Vogal maiúscula', regex: /[AEIOU]/},
+    {message: 'Vogal minuscula', regex: /[aeiou]/},
+    {message: 'Consoante maiúscula', regex: /[A-Z]/},
+    {message: 'Consoante minúsculo', regex: /[a-z]/},
+    {message: 'É um número', regex: /\d/},
+  ]
+
+  for(let testRegex of regexs) {
+    const { message, regex } = testRegex
+    if(regex.test(caractere)) {
+      return message
+    }
+  }
+}
+
+console.log(indentifyCaractere(caractere))
