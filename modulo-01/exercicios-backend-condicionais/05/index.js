1b70ac521b3cbbc7b996d593cb56dc2fd83148b6
chain <klaus.mika1052@gmail.com>
2023-06-20 18:06:08 -0300

    feat: completed exercise-05

--- modulo-01/exercicios-backend-condicionais/05/index.js
+++ modulo-01/exercicios-backend-condicionais/05/index.js
@@ -1,3 +1,12 @@
 const alturaEmCm = 185;
 
 //seu código aqui
+function checkHeight(alturaEmCm) {
+  if(alturaEmCm >= 180) {
+    return '**APROVADO**'
+  } else {
+    return '**REPROVADO**'
+  }
+}
+
+console.log(checkHeight(alturaEmCm))
