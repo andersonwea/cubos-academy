1d39bbe4733a5e962c6285df0e3c36c04287e9b5
chain <klaus.mika1052@gmail.com>
2023-06-20 18:27:31 -0300

    feat: completed exercise-06

--- modulo-01/exercicios-backend-condicionais/06/index.js
+++ modulo-01/exercicios-backend-condicionais/06/index.js
@@ -1,3 +1,23 @@
-const alturaEmCm = 185;
+const alturaEmCm = 205;
+
+const positions = {
+  185: 'LÍBERO',
+  195: 'PONTEIRO',
+  205: 'OPOSTO',
+  Infinity: 'CENTRAL'
+}
 
 //seu código aqui
+function takePlayerPosition(alturaEmCm) {
+  if(alturaEmCm < 180) {
+    return '***REPROVADO***'
+  }
+
+  for(let position in positions) {
+    if(alturaEmCm <= position) {
+      return positions[position]
+    }
+  }
+}
+
+console.log(takePlayerPosition(alturaEmCm))
