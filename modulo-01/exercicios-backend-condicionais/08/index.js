8e27320aa6257787e8b27db7f12caf06f5cd8f78
chain <klaus.mika1052@gmail.com>
2023-06-20 18:46:03 -0300

    feat: completed exercise-08

--- modulo-01/exercicios-backend-condicionais/08/index.js
+++ modulo-01/exercicios-backend-condicionais/08/index.js
@@ -3,3 +3,14 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+function checkEntryPermition(idade, possuiPatologia, altura, ehEstudante) {
+  if(idade < 12 || idade > 65 || possuiPatologia || altura < 150) {
+    return 'ACESSO NEGADO'
+  }
+
+  const entryValue = ehEstudante || idade < 18 ? '10 reais' : '20 reais'
+
+  return entryValue
+}
+
+console.log(checkEntryPermition(idade, possuiPatologia, altura, ehEstudante))
