cdae948ca1880f6fddee006120f0d91a554845f0
chain <klaus.mika1052@gmail.com>
2023-06-20 20:39:50 -0300

    feat: completed exercise-11

--- modulo-01/exercicios-backend-condicionais/11/index.js
+++ modulo-01/exercicios-backend-condicionais/11/index.js
@@ -6,3 +6,20 @@ const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+
+function calStudentFunding(rendaMensalEmCentavos, mesesDecorridos, totalJaPagoPeloAluno) {
+  let monthPayment = 0
+
+  if(rendaMensalEmCentavos < 200000) {
+    return `O valor da parcela desse mês é R$ ${monthPayment} reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.`
+  } else if (mesesDecorridos > 60 || totalJaPagoPeloAluno > 1800000) {
+    monthPayment = 0
+  } else {
+    monthPayment = rendaMensalEmCentavos * 0.18 
+  }
+
+
+  return `O valor da parcela desse mês é R$ ${monthPayment / 100} reais`
+}
+
+console.log(calStudentFunding(rendaMensalEmCentavos, mesesDecorridos, totalJaPagoPeloAluno))
