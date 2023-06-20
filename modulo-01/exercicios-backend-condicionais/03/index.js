312bf79394eb1a96e1499ca41128fefff40b01ec
chain <klaus.mika1052@gmail.com>
2023-06-20 17:15:56 -0300

    feat: completed exercise-03

--- modulo-01/exercicios-backend-condicionais/03/index.js
+++ modulo-01/exercicios-backend-condicionais/03/index.js
@@ -2,3 +2,12 @@ const ladoA = 3;
 const ladoB = 3;
 
 //seu código aqui
+function isDouble(ladoA, ladoB) {
+  if(ladoA === ladoB) {
+    return "SIM"
+  } else {
+    return "NÃO"
+  }
+}
+
+console.log(isDouble(ladoA, ladoB))
