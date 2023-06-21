017e294f755c094e58810fb748f964fb37025a2b
chain <klaus.mika1052@gmail.com>
2023-06-20 21:11:33 -0300

    feat: completed exercise-15

--- modulo-01/exercicios-backend-condicionais/15/index.js
+++ modulo-01/exercicios-backend-condicionais/15/index.js
@@ -1,4 +1,19 @@
 const primeiroNome = "Mario";
 const sobrenome = "";
-const apelido = "";
+const apelido = "grandão";
 
+function displayName(primeiroNome, sobrenome, apelido) {
+  if(!primeiroNome) {
+    return 'Nome é um campo obrigatório'
+  } 
+  
+  if(apelido) {
+    return apelido
+  } else if (sobrenome) {
+    return primeiroNome + ' ' + sobrenome
+  } else {
+    return primeiroNome
+  }
+}
+
+console.log(displayName(primeiroNome, sobrenome, apelido))
