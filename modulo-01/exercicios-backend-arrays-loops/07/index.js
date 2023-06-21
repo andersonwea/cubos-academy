4b86823f833d5827933f0023826aa077ea1b9005
chain <klaus.mika1052@gmail.com>
2023-06-21 19:19:06 -0300

    feat: completed exercise-07

--- modulo-01/exercicios-backend-arrays-loops/07/index.js
+++ modulo-01/exercicios-backend-arrays-loops/07/index.js
@@ -1,1 +1,15 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+
+function filterNameWithLetterA(nomes) {
+  let namesWithLetterA = []
+
+  for(name of nomes) {
+    if(name.charAt(0) === 'A' || name.charAt(0) === 'a') {
+      namesWithLetterA.push(name)
+    }
+  }
+
+  return namesWithLetterA
+}
+
+console.log(filterNameWithLetterA(nomes))
