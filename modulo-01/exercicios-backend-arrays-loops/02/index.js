b5ae380f89d4e2120f4c2089598822509f35ce65
chain <klaus.mika1052@gmail.com>
2023-06-21 19:00:15 -0300

    feat: completed exercise-02

--- modulo-01/exercicios-backend-arrays-loops/02/index.js
+++ modulo-01/exercicios-backend-arrays-loops/02/index.js
@@ -1,1 +1,21 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+
+function countLetters(letras) {
+  let message = ''
+  let count = 0
+
+  for(word of letras) {
+    if(word === 'E' || word === 'e') {
+      count += 1
+      message = `Foram encontradas ${count} letras "E" ou "e"`
+    }
+  }
+
+  if(!count) {
+    message = `Nenhuma letra "E" ou "e" foi encontrada`
+  }
+
+  return message
+}
+
+console.log(countLetters(letras))
