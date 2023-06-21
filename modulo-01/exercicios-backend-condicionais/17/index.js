3779ce74229e7bb1469d06895706f74e922fd4f9
chain <klaus.mika1052@gmail.com>
2023-06-21 11:44:05 -0300

    feat: completed exercise-17

--- modulo-01/exercicios-backend-condicionais/17/index.js
+++ modulo-01/exercicios-backend-condicionais/17/index.js
@@ -6,3 +6,20 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+function purchasePaymentStatus(valorDoProduto, quantidadeDoParcelamento, valorPago) {
+  const amountPaidInCents = valorPago * 100
+  const needtoPay = valorDoProduto - amountPaidInCents
+
+  if(needtoPay <= 0) {
+    return 'O produto já foi pago'
+  }
+
+  const monthyPayment = valorDoProduto / quantidadeDoParcelamento
+
+  const monthyPaymentMissing = quantidadeDoParcelamento - amountPaidInCents / monthyPayment 
+
+  return `Restam ${monthyPaymentMissing} parcelas de R$ ${monthyPayment / 100}`
+}
+
+console.log(purchasePaymentStatus(valorDoProduto, quantidadeDoParcelamento, valorPago))
