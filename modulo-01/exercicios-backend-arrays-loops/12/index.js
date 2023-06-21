cba3281c04157742652f89cfd020ab90bb307fd9
chain <klaus.mika1052@gmail.com>
2023-06-21 19:46:50 -0300

    feat: completed exercise-12

--- modulo-01/exercicios-backend-arrays-loops/12/index.js
+++ modulo-01/exercicios-backend-arrays-loops/12/index.js
@@ -1,3 +1,12 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+function queueControl(filaDeDentro, filaDeFora) {
+  while(filaDeDentro.length < 5) {
+    filaDeDentro.push(filaDeFora.shift())
+  }
+
+  return {filaDeDentro, filaDeFora}
+}
+
+console.log(queueControl(filaDeDentro, filaDeFora))
