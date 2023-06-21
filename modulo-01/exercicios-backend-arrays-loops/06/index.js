bff3528e778b58846286ede0a7caf1127797394a
chain <klaus.mika1052@gmail.com>
2023-06-21 19:15:15 -0300

    feat: completed exercise-06

--- modulo-01/exercicios-backend-arrays-loops/06/index.js
+++ modulo-01/exercicios-backend-arrays-loops/06/index.js
@@ -1,2 +1,15 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
 
+function sumOddNumber(numeros) {
+  let sum = 0
+
+  for(number of numeros) {
+    if(number % 2 === 0) {
+      sum += number
+    }
+  }
+
+  return sum
+}
+
+console.log(sumOddNumber(numeros))
