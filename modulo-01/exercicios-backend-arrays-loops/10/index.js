250e8a91aee6233891fc2044c6a93391a6c6bd68
chain <klaus.mika1052@gmail.com>
2023-06-21 19:36:31 -0300

    feat: completed exercise-10

---
+++ modulo-01/exercicios-backend-arrays-loops/10/index.js
@@ -0,0 +1,18 @@
+const numeros = [8, 11, 4, 1];
+
+function maxDifference(numeros) {
+  let max = numeros[0]
+  let min = numeros[0]
+
+  for(number of numeros) {
+    if(number < min) {
+      min = number
+    } else if(number > max) {
+      max = number
+    }
+  }
+
+  return max - min
+}
+
+console.log(maxDifference(numeros))
