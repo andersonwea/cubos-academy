8c76f7194d9e331767f34781b56c89cacc754811
chain <klaus.mika1052@gmail.com>
2023-06-20 21:17:33 -0300

    feat: completed exercise-16

--- modulo-01/exercicios-backend-condicionais/16/index.js
+++ modulo-01/exercicios-backend-condicionais/16/index.js
@@ -3,3 +3,13 @@ const idadeDoAluno = 16;
 
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
+
+function schollInscribe(idadeDoAluno, possuiResponsavel) {
+  if (idadeDoAluno > 18 || idadeDoAluno < 18 && possuiResponsavel) {
+    return 'Rematrícula realizada com sucesso'
+  } else { 
+    return 'Não é possível fazer a rematrícula'
+  }
+}
+
+console.log(schollInscribe(idadeDoAluno, possuiResponsavel))
