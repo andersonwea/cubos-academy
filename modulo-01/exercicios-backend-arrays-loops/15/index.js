6dfce913a43d4b727bfaf3d9126840d587497b41
chain <klaus.mika1052@gmail.com>
2023-06-21 19:59:33 -0300

    feat: completed exercise-15

--- modulo-01/exercicios-backend-arrays-loops/15/index.js
+++ modulo-01/exercicios-backend-arrays-loops/15/index.js
@@ -2,3 +2,16 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+function sliptOddOfEven(original) {
+  for(number of original) {
+    if(number % 2 == 0) {
+      pares.push(number)
+    } else {
+      impares.push(number)
+    }
+  }
+
+  return {pares, impares}
+}
+
+console.log(sliptOddOfEven(original))
