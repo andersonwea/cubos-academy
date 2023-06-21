2ad174e49dc143f69758c9a0a0aecf93e392e8ce
chain <klaus.mika1052@gmail.com>
2023-06-21 19:04:31 -0300

    feat: completed exercise-03

--- modulo-01/exercicios-backend-arrays-loops/03/index.js
+++ modulo-01/exercicios-backend-arrays-loops/03/index.js
@@ -1,2 +1,17 @@
 const numeros = [54, 22, 14, 87, 10, 284];
 
+function findNumber(numeros) {
+  let index = 0
+
+  for(number of numeros) {
+    if(number === 10) {
+      break
+    }
+    
+    index += 1
+  }
+
+  return index
+}
+
+console.log(findNumber(numeros))
