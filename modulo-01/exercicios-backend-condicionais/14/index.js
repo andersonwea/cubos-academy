c379799f39fc1b8b1a49b0b89c9ab171dd91d1f1
chain <klaus.mika1052@gmail.com>
2023-06-20 21:06:02 -0300

    feat: completed exercise-14

--- modulo-01/exercicios-backend-condicionais/14/index.js
+++ modulo-01/exercicios-backend-condicionais/14/index.js
@@ -1,3 +1,20 @@
 //quantidade de água ingerida em litro.
-const quantidadeDeAguaIngerida = 2;
+const quantidadeDeAguaIngerida = 1;
 
+function waterConsumeControl(quantidadeDeAguaIngerida) {
+  const waterControls = [
+    {amount: 1.5, risk: 'ALTO', message: 'Você está ingerindo pouquissima água, beba mais água!'},
+    {amount: 3.0, risk: 'MODERADO', message: 'Você está ingerindo pouca água, beba mais!'},
+    {amount: Infinity, risk: 'BAIXO', message: 'Você está ingerindo uma boa quantidade de água, parabéns!'}
+  ]
+
+  for(let waterControl of waterControls) {
+    const { amount, risk, message } = waterControl
+
+    if(quantidadeDeAguaIngerida < amount) {
+      return `Risco ${risk} - ${message}`
+    }
+  }
+}
+
+console.log(waterConsumeControl(quantidadeDeAguaIngerida))
