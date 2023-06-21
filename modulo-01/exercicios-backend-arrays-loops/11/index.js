8ebbc3c6f79f3af1c63eabdcd49029d6bb4424a8
chain <klaus.mika1052@gmail.com>
2023-06-21 19:40:59 -0300

    feat: completed exercise-11

--- modulo-01/exercicios-backend-arrays-loops/11/index.js
+++ modulo-01/exercicios-backend-arrays-loops/11/index.js
@@ -1,2 +1,15 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
 
+function filterNumbers(orginal) {
+  let filteredNumbers = []
+
+  for(number of original) {
+    if(number >= 10 && number <= 20 || number > 100) {
+      filteredNumbers.push(number)
+    }
+  }
+
+  return filteredNumbers
+}
+
+console.log(filterNumbers(original));
