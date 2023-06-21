2722a8a63f064737aad9a7786a52bfa70afaa8d8
chain <klaus.mika1052@gmail.com>
2023-06-21 19:13:09 -0300

    feat: completed exercise-05

--- modulo-01/exercicios-backend-arrays-loops/05/index.js
+++ modulo-01/exercicios-backend-arrays-loops/05/index.js
@@ -1,1 +1,15 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+
+function filterOddNumbers(original) {
+  let oddNumbers = []
+  
+  for(number of original) {
+    if(number % 2 === 0) {
+      oddNumbers.push(number)
+    }
+  }
+
+  return oddNumbers
+}
+
+console.log(filterOddNumbers(original))
