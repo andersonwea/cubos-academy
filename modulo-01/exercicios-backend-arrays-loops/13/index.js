140e9ad2cb25acd1a75cd5d3bc09cfd737787e98
chain <klaus.mika1052@gmail.com>
2023-06-21 19:50:23 -0300

    feat: completed exercise-13

--- modulo-01/exercicios-backend-arrays-loops/13/index.js
+++ modulo-01/exercicios-backend-arrays-loops/13/index.js
@@ -1,2 +1,15 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+function checkCircuitBreakers(disjuntores) {
+  let index = 0
+  
+  for(circuitBreaker of disjuntores) {
+    if(circuitBreaker) {
+      console.log(index)
+    }
+    
+    index += 1
+  }
+}
+
+checkCircuitBreakers(disjuntores)
