b4213f72d606799e680518516c9d9d684117d1ab
chain <klaus.mika1052@gmail.com>
2023-06-21 19:27:46 -0300

    feat: completed exercise-09

--- modulo-01/exercicios-backend-arrays-loops/09/index.js
+++ modulo-01/exercicios-backend-arrays-loops/09/index.js
@@ -1,2 +1,12 @@
 
 const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"];
+
+function onderQueue(filaDePedidos) {
+  filaDePedidos.push('pedido 15')
+
+  filaDePedidos.shift()
+
+  return filaDePedidos
+}
+
+console.log(onderQueue(filaDePedidos))
