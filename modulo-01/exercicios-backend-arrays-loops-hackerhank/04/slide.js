c4167d9a0d1fe9b70e21c65fb2c31f12769c95d3
chain <klaus.mika1052@gmail.com>
2023-06-26 17:39:11 -0300

    feat: completed exercise 04-slide

---
+++ modulo-01/exercicios-backend-arrays-loops-hackerhank/04/slide.js
@@ -0,0 +1,21 @@
+function solucao(sequencia) {
+	let activeSlide = 0
+  
+  for(let click of sequencia) {
+    if(click === '>') {
+     activeSlide += 1
+    } 
+
+    if (click === '<') {
+     activeSlide -= 1
+    } 
+    
+    if(activeSlide > 6) {
+      activeSlide = 0
+    } else if (activeSlide < 0) {
+      activeSlide = 6
+    }
+  }
+  
+  console.log(activeSlide)
+}
