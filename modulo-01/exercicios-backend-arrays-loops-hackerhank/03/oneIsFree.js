4dae97977bba24b13941f3ac8f53e55321a50bdc
chain <klaus.mika1052@gmail.com>
2023-06-26 17:10:51 -0300

    feat: completed exercise 03-oneIsFree

---
+++ modulo-01/exercicios-backend-arrays-loops-hackerhank/03/oneIsFree.js
@@ -0,0 +1,18 @@
+function solucao(precos) {
+  let total = 0
+  let min = precos[0]
+  
+  for(let preco of precos) {
+    if(preco < min) {
+      min = preco
+    }
+    
+    total += preco
+  }
+  
+  if(precos.length >= 5) {
+    total = total - min
+  }
+  
+  console.log(total)  
+}
