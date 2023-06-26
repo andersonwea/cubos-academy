193f7cfebab175bc37f67426c5b3c119e4e9a654
chain <klaus.mika1052@gmail.com>
2023-06-26 20:14:06 -0300

    feat: completed exercise 05-lessLoss

---
+++ modulo-01/exercicios-backend-arrays-loops-hackerhank/05/index.js
@@ -0,0 +1,21 @@
+
+function solucao(precos) {
+  let difference = 0
+  let minDifference = 0
+
+  for(let i = 0; i < precos.length; i++) {
+    for(let j = i + 1; j < precos.length; j++) {
+      if(precos[i] > precos[j]) {
+        difference = precos[i] - precos[j]
+        
+        if(!minDifference || difference < minDifference) {
+          minDifference = difference
+        }
+      }
+    }
+  }
+
+  console.log(minDifference)
+}
+
+solucao([30, 10, 54, 76, 1, 4, 35])
