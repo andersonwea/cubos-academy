70a773664b2ec5f1e5ab443519d7c8e6884735ed
chain <klaus.mika1052@gmail.com>
2023-06-27 16:30:57 -0300

    feat: completed exercise-03

---
+++ modulo-01/exercicios-backend-objetos/03/index.js
@@ -0,0 +1,11 @@
+const user = {
+  name: 'Anderson',
+  age: 30,
+  occupation: 'Engenheiro de software',
+  favoriteColor: 'azul'
+}
+
+const { name, age, occupation, favoriteColor} = user
+
+console.log(`Olá... Eu sou ${name} e tenho ${age} anos de idade.
+ Sou ${occupation} e minha cor favorita é ${favoriteColor}!`)
