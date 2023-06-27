231356a89a868657ade89c810a0c99841ff53bbb
chain <klaus.mika1052@gmail.com>
2023-06-26 21:01:14 -0300

    feat: completed exercise 06-brazilWithP

---
+++ modulo-01/exercicios-backend-arrays-loops-hackerhank/06/index.js
@@ -0,0 +1,18 @@
+function solucao(primeiraLetra, segundaLetra, palavras) {
+  const firstTwoLettersToSearch = `${primeiraLetra}${segundaLetra}`
+  let isWordFound = false
+  
+  for(let word of palavras) {
+    let firstTwoLettersWord = word[0] + word[1]
+
+    if(firstTwoLettersToSearch === firstTwoLettersWord) {
+      console.log(word)
+      isWordFound = true
+    }
+  }
+  if(!isWordFound) {
+    console.log('NENHUMA')  
+  }
+}
+
+solucao('a', 'v', ['aveia','manha', 'ave'])
