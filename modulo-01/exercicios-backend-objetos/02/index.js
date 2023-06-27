af7715af763486d8375f0e1447950091fe4214e5
chain <klaus.mika1052@gmail.com>
2023-06-27 16:21:53 -0300

    feat: completed exercise-02

---
+++ modulo-01/exercicios-backend-objetos/02/index.js
@@ -0,0 +1,26 @@
+const cars = [
+  {
+    brand: 'Volkswagen',
+    model: 'Gol',
+    year: 2014,
+    color: 'Red',
+    doorsQuantity: 4,
+    automatic: true
+ },
+  {
+    brand: 'Volkswagen',
+    model: 'Golf',
+    year: 2022,
+    color: 'White',
+    doorsQuantity: 4,
+    automatic: true
+ },
+ {
+  brand: 'Bmw',
+  model: '320i',
+  year: 2023,
+  color: 'white',
+  doorsQuantity: 4,
+  automatic: true
+}
+]
