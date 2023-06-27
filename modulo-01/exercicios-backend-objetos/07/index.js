b3606cc12b119ed546af826442c906026f9b78a8
chain <klaus.mika1052@gmail.com>
2023-06-27 17:03:54 -0300

    feat: completed exercise-07

---
+++ modulo-01/exercicios-backend-objetos/07/index.js
@@ -0,0 +1,38 @@
+const book = {
+  name: 'O poder do hábito',
+  chapters: [
+      {
+          name: "O loop do hábito",
+      },
+      {
+          name: "O cérebro ansioso",
+      },
+      {
+          name: "A regra de ouro da mudança de hábito",
+      },
+      {
+          name: "Hábitos angulares, ou a balada de Paul O`neill",
+      },
+      {
+          name: "Starbucks e o hábito do sucesso",
+      },
+      {
+          name: "O poder de uma crise",
+      },
+      {
+          name: "Como a target sabe o que você quer antes que você saiba",
+      },
+      {
+          name: "A Saddleback Church e o boicote aos ônibus de Montgomery",
+      },
+      {
+          name: "A neorologia do livre-arbítrio",
+      },
+  ]
+}
+
+book.chapters.forEach((chapter, index) => {
+  chapter.number = index + 1
+})
+
+console.log(book)
