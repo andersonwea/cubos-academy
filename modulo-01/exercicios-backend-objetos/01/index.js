8da249f0158550c6dc30ef78e14287bf95f929e0
chain <klaus.mika1052@gmail.com>
2023-06-27 16:12:33 -0300

    feat: completed exercise-01

---
+++ modulo-01/exercicios-backend-objetos/01/index.js
@@ -0,0 +1,18 @@
+const address = {
+  street: 'Rua das magnólias',
+  number: 350,
+  complement: 'Casa',
+  neighborhood: 'Centro',
+  zipcode: '07995-256',
+  city: 'São Paulo',
+  state: 'São Paulo',
+  country: 'Brasil'
+}
+
+const user = {
+  name: 'John Doe',
+  email: 'john.doe@gmail.com',
+  telephone: '11955632587',
+  birthDate: '18-10-1995',
+  address: address
+}
