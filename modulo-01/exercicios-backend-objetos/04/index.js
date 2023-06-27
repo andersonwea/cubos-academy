195caa29c909270ab50b16e5c00771fb1ec2c0a9
chain <klaus.mika1052@gmail.com>
2023-06-27 16:41:50 -0300

    feat: completed exercise-04

---
+++ modulo-01/exercicios-backend-objetos/04/index.js
@@ -0,0 +1,30 @@
+const users = [
+  {
+      name: "João",
+      age: 25,
+  },
+  {
+      name: "Ana",
+      age: 18,
+  },
+  {
+      name: "Beatriz",
+      age: 15,
+  },
+  {
+      name: "Carlos",
+      age: 16,
+  },
+  {
+      name: "Antonio",
+      age: 32,
+  },
+]
+
+users.forEach(user => {
+  const adult = user.age >= 18
+  
+  user.adult = adult
+})
+
+console.log(users)
