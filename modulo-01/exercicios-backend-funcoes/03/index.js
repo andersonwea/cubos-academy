232f2d98982d9964179508e2460109b4ddcff53f
chain <klaus.mika1052@gmail.com>
2023-06-28 18:09:30 -0300

    feat: completed exercise 03

--- modulo-01/exercicios-backend-funcoes/03/index.js
+++ modulo-01/exercicios-backend-funcoes/03/index.js
@@ -1,17 +1,42 @@
-const carrinho = {
-    nomeDoCliente: "Guido Bernal",
-    produtos: [
-        {
-            id: 1,
-            nome: "Camisa",
-            qtd: 3,
-            precoUnit: 3000
-        },
-        {
-            id: 2,
-            nome: "Bermuda",
-            qtd: 2,
-            precoUnit: 5000
-        }
-    ]
+const cart = {
+  clientName: "Guido Bernal",
+  products: [
+      {
+          id: 1,
+          name: "Camisa",
+          qty: 3,
+          unitPrice: 3000
+      },
+      {
+          id: 2,
+          name: "Bermuda",
+          qty: 2,
+          unitPrice: 5000
+      }
+  ]
 }
+
+function printCartSummary(cart) {
+  const { clientName, products } = cart
+
+  const { totalItems, totalToPay } = products.reduce((checkout, product) => {
+    checkout.totalItems += product.qty
+    checkout.totalToPay += product.unitPrice
+
+    return checkout
+  }, {totalItems: 0, totalToPay: 0})
+
+  const totalToPayInReal = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(totalToPay / 100)
+
+  return (
+    `
+      Client: ${clientName}
+      Total de itens: ${totalItems} itens
+      Total a pagar: ${totalToPayInReal}`
+  )
+}  
+
+console.log(printCartSummary(cart))
