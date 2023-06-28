7600a17a8dce1e42e95689d91d7a222d32d38657
chain <klaus.mika1052@gmail.com>
2023-06-28 19:35:03 -0300

    feat: completed exercise 03

--- modulo-01/exercicios-backend-funcoes/03/index.js
+++ modulo-01/exercicios-backend-funcoes/03/index.js
@@ -13,30 +13,96 @@ const cart = {
           qty: 2,
           unitPrice: 5000
       }
-  ]
+  ],
+  total() {
+    const { totalItems, totalToPay } = this.products.reduce((checkout, product) => {
+      checkout.totalItems += product.qty
+      checkout.totalToPay += product.unitPrice * product.qty
+  
+      return checkout
+    }, {totalItems: 0, totalToPay: 0})
+
+    return {totalItems, totalToPay}
+  },
+
+  printSummary() {
+    const clientName = this.clientName
+  
+    const { totalItems, totalToPay } = this.total()
+  
+    const totalToPayInReal = convertPriceInReal(totalToPay)
+  
+    return (
+      `
+        Client: ${clientName}
+        Total de itens: ${totalItems} itens
+        Total a pagar: ${totalToPayInReal}`
+    )
+  },
+
+  addProduct(product) {
+    this.products.push(...product)
+  },
+
+  printDetails() {
+    const clientName = this.clientName
+    const { totalItems, totalToPay} = this.total()
+
+    const totalToPayInReal = convertPriceInReal(totalToPay)
+
+    const productsDetail = this.products.reduce((productsDetail, product, index) => {
+      const { name, qty, unitPrice } = product
+      const unitPriceInReal = convertPriceInReal(unitPrice)
+
+      productsDetail.push({ detail: `Item ${index + 1} - ${name} - ${qty} und - ${unitPriceInReal}` })
+
+      return productsDetail
+    }, [])
+
+    console.log(
+      `
+        Cliente: ${clientName}
+        ${productsDetail.map(({detail}) => {
+          return (
+            `
+            ${detail}`
+          )
+        })}
+
+        Total de itens: ${totalItems} itens
+        Total a pagar: ${totalToPayInReal}
+      `
+    )
+  }
 }
 
-function printCartSummary(cart) {
-  const { clientName, products } = cart
-
-  const { totalItems, totalToPay } = products.reduce((checkout, product) => {
-    checkout.totalItems += product.qty
-    checkout.totalToPay += product.unitPrice
-
-    return checkout
-  }, {totalItems: 0, totalToPay: 0})
-
-  const totalToPayInReal = new Intl.NumberFormat('pt-BR', {
+function convertPriceInReal(priceInCents) {
+  const priceInReal = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL'
-  }).format(totalToPay / 100)
+  }).format(priceInCents / 100)
 
-  return (
-    `
-      Client: ${clientName}
-      Total de itens: ${totalItems} itens
-      Total a pagar: ${totalToPayInReal}`
-  )
-}  
+  return priceInReal
+}
 
-console.log(printCartSummary(cart))
+
+const newShorts = {
+  id: 2,
+  name: "Bermuda",
+  qty: 3,
+  unitPrice: 5000
+}
+
+const newSneaker = {
+  id: 3,
+  name: "Tenis",
+  qty: 1,
+  unitPrice: 10000
+}
+
+
+cart.addProduct([newShorts, newSneaker])
+cart.printDetails()
+
+
+
