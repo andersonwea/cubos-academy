db37989ea86ac146c04b34306ecf054285b35950
chain <klaus.mika1052@gmail.com>
2023-06-28 16:36:07 -0300

    feat: completed exercise 01

--- modulo-01/exercicios-backend-funcoes/01/index.js
+++ modulo-01/exercicios-backend-funcoes/01/index.js
@@ -1,28 +1,49 @@
-const prova = {
-    aluno: "João",
-    materia: "Português",
-    valor: 10,
-    questoes: [
-        {
-            resposta: "a",
-            correta: "b"
-        },
-        {
-            resposta: "c",
-            correta: "c"
-        },
-        {
-            resposta: "e",
-            correta: "b"
-        },
-        {
-            resposta: "b",
-            correta: "b"
-        },
-        {
-            resposta: "c",
-            correta: "c"
-        }
-    ]
-};
+const test = {
+  student: "João",
+  subject: "Português",
+  value: 10,
+  questions: [
+      {
+          answer: "a",
+          correct: "b"
+      },
+      {
+          answer: "c",
+          correct: "c"
+      },
+      {
+          answer: "e",
+          correct: "b"
+      },
+      {
+          answer: "b",
+          correct: "b"
+      },
+      {
+          answer: "c",
+          correct: "c"
+      }
+  ]
+}
 
+function checkTest(test) {
+  const { student, value, questions} = test
+
+  const correctAnswers = questions.reduce((correctAnswer, answer) => {
+    const isCorrect = answer.answer === answer.correct
+
+    if(isCorrect) {
+      correctAnswer += 1
+    }
+
+    return correctAnswer
+  }, 0)
+
+  const poitsPerCorrectAnswer = value / questions.length
+
+  const grade = poitsPerCorrectAnswer * correctAnswers
+
+  return `O aluno(a) ${student} acertou ${correctAnswers} e obteve nota ${grade}`
+}
+
+console.log(checkTest(test))
