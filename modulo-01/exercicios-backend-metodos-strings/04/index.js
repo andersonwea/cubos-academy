dfd159ca5f437eadfae58ddfd71e280460d3b8c3
chain <klaus.mika1052@gmail.com>
2023-06-29 18:22:46 -0300

    feat: completed exercise-04

--- modulo-01/exercicios-backend-metodos-strings/04/index.js
+++ modulo-01/exercicios-backend-metodos-strings/04/index.js
@@ -1,3 +1,11 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+identificador = identificador.padStart(6, '0')
+nome = nome.split(' ').map(word => word.charAt(0).toUpperCase().concat(word.substring(1))).join(' ');
+email = email.trim()
+
+console.log(identificador)
+console.log(nome)
+console.log(email)
--- modulo-01/exercicios-backend-metodos-strings/05/index.js
+++ modulo-01/exercicios-backend-metodos-strings/05/index.js
@@ -1,1 +1,11 @@
-const numeroCartao = '1111222233334444';
+const cardNumber = '1111222233334444';
+
+function hiddenCardNumber(cardNumber) {
+  const regex = /(\w{4})(\w{8})(\w{4})/g
+
+  const cardNumberHidden = cardNumber.replace(regex, '$1********$3')
+
+  return cardNumberHidden
+}
+
+console.log(hiddenCardNumber(cardNumber))
