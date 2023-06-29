ff91851a592888b015e4d101de60f6bcbaa248f1
chain <klaus.mika1052@gmail.com>
2023-06-29 17:49:14 -0300

    feat: completed exercise-03

--- modulo-01/exercicios-backend-metodos-strings/03/index.js
+++ modulo-01/exercicios-backend-metodos-strings/03/index.js
@@ -1,1 +1,11 @@
-const texto = "Aprenda programar do zero na Cubos Academy";
+const text = "Aprenda programar do zero na Cubos Academy";
+
+function formatText(text) {
+  const regex = /\s/g
+
+  const formatedText = text.replace(regex, '-').toLowerCase();
+
+  return formatedText
+}
+
+console.log(formatText(text))
