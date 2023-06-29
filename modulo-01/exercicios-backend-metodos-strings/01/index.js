01560384e90574e0b1e839a3749ac42573f2a25e
chain <klaus.mika1052@gmail.com>
2023-06-29 17:10:45 -0300

    feat: completed exercise-01

--- modulo-01/exercicios-backend-metodos-strings/01/index.js
+++ modulo-01/exercicios-backend-metodos-strings/01/index.js
@@ -1,1 +1,14 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comment = "Esse COVID é muito perigoso!";
+
+function filterComments(comment) {
+  const regex = new RegExp(/pandemia|covid/i)
+  const isBlocked = regex.test(comment)
+
+  if(isBlocked) {
+    return 'Comentário bloqueado por conter palavras proibidas'
+  }
+
+  return 'Comentário autorizado'
+}
+
+console.log(filterComments(comment))
