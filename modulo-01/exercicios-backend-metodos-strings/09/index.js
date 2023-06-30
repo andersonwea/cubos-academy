893f456f96042687e407e3fd2c708d94c17483fa
chain <klaus.mika1052@gmail.com>
2023-06-30 14:26:29 -0300

    feat: completed exercise-09

--- modulo-01/exercicios-backend-metodos-strings/09/index.js
+++ modulo-01/exercicios-backend-metodos-strings/09/index.js
@@ -1,1 +1,9 @@
-const nome = 'Guido Cerqueira';
+const nome = 'Guido Cerqueira'
+
+function nicknameGenerator(nome) {
+  let nickname = nome.replace(' ', '').toLowerCase().slice(0, 12)
+
+  return '@' + nickname
+}
+
+console.log(nicknameGenerator(nome))
