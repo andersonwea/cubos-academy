2b7e3e444918dd6c524f86cb7d1d611a6388b52e
chain <klaus.mika1052@gmail.com>
2023-06-30 14:14:12 -0300

    feat: completed exercise-08

--- modulo-01/exercicios-backend-metodos-strings/07/index.js
+++ modulo-01/exercicios-backend-metodos-strings/07/index.js
@@ -1,1 +1,30 @@
-const email = "aluno@cubos.academy";
+const emails = [
+  'jose@cubos.academy',
+  'jose@cubos.academy.br',
+  'jose.messias@cubos.academy',
+  'jose.messias@cubos.io',
+  'jose@cubos',
+  'jose.messias@cubos',
+  'jose.messias@.',
+  'jose.@cubos',
+  '.@',
+  '@.',
+  'jose.messias@cubos.',
+  '.messias@cubos.',
+  '.messias@cubo',
+]
+
+function validateEmail(email) {
+  const regex = new RegExp(/^[^.].*@.*[^.]$/)
+
+  let isValidEmail = regex.test(email) 
+
+  if(!isValidEmail) {
+    return 'E-mail inválido'
+  }
+    
+  return 'E-mail válido'
+  
+}
+
+emails.forEach(email => console.log(validateEmail(email)))
--- modulo-01/exercicios-backend-metodos-strings/08/index.js
+++ modulo-01/exercicios-backend-metodos-strings/08/index.js
@@ -1,1 +1,11 @@
 const cpf = "011.022.033-44";
+
+function removeSpecialCaracters(cpf) {
+  const regex = new RegExp(/\W|\D/g)
+
+  const formatedCpf = cpf.replace(regex, '')
+
+  return formatedCpf
+}
+
+console.log(removeSpecialCaracters(cpf))
