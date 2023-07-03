8e3b71e2a98e0d8d0b6062cf33644c29be4a6cbe
chain <klaus.mika1052@gmail.com>
2023-07-03 16:59:46 -0300

    feat: completed exercise-10

--- modulo-01/exercicios-backend-metodos-strings/10/index.js
+++ modulo-01/exercicios-backend-metodos-strings/10/index.js
@@ -1,1 +1,15 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.png';
+
+function isImage(nomeArquivo) {
+  const allowedImages = ['gif', 'jpg', 'png', 'jpeg']
+
+  const result = !!allowedImages.find(ext => nomeArquivo.includes(ext))
+
+  if(!result) {
+    return 'Arquivo inválido'
+  }
+
+  return 'Arquivo válido'
+}
+
+console.log(isImage(nomeArquivo))
