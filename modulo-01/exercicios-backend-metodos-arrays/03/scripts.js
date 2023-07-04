cbd5ad6a3da14321add2538498ecb94d6c5d657f
chain <klaus.mika1052@gmail.com>
2023-07-04 20:29:24 -0300

    feat: completed exercise-03

---
+++ modulo-01/exercicios-backend-metodos-arrays/03/scripts.js
@@ -0,0 +1,10 @@
+const cars = ['Ford Ká', 'Ranger', 'Hilux', 'Corola', 'Fusca', 'Chevete', 'Brasilia'];
+const position = 3;
+
+function carsGroup(cars, position) {
+  const carGroup = cars.slice(position, position + 3)
+
+  return carGroup.join(' - ')
+}
+
+console.log(carsGroup(cars, position))
