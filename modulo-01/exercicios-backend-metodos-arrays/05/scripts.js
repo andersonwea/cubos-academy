47726ded3fc99e487ce2cd7920d7c36b2f5ea6a0
chain <klaus.mika1052@gmail.com>
2023-07-04 20:52:03 -0300

    feat: completed exercise-05

--- modulo-01/exercicios-backend-metodos-arrays/05/scripts.js
+++ modulo-01/exercicios-backend-metodos-arrays/05/scripts.js
@@ -1,1 +1,25 @@
-const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+const patients = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+function schedulePatient(patients, name) {
+  const updatedQueue = patients.filter(patient => patient != name)
+  updatedQueue.push(name)
+
+  return updatedQueue.join(', ')
+}
+
+function attendPacient(patients) {
+  patients.shift()
+
+  return patients.join(', ')
+}
+
+function cancelAppointment(patients, name) {
+  const updatedQueue = patients.filter(patient => patient != name)
+
+  return updatedQueue.join(', ')
+}
+
+console.log(schedulePatient(patients, 'Ana'))
+console.log(attendPacient(patients))
+console.log(cancelAppointment(patients, 'Bárbara'))
+
