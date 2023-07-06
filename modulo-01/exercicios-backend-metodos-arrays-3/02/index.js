5e1853b4f11aad19d860559c505c440952ea2134
chain <klaus.mika1052@gmail.com>
2023-07-06 18:01:57 -0300

    fix: exercise-02, always return the last one in the array

--- modulo-01/exercicios-backend-metodos-arrays-3/02/index.js
+++ modulo-01/exercicios-backend-metodos-arrays-3/02/index.js
@@ -2,14 +2,12 @@ const cities = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'
 
 function greatestLength(cities) {
   const greatest = cities.reduce((greatest, word) => {
-    greatest = cities[0]
-
     if (word.length > greatest.length) {
       greatest = word
     }
 
     return word
-  })
+  }, cities[0])
 
   return greatest
 }
