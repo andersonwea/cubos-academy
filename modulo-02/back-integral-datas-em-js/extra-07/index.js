72779b0dd40bd144f6b2d79cdb307a9b55bc885a
chain <klaus.mika1052@gmail.com>
2023-07-31 12:17:13 -0300

    completed exercise-07

---
+++ modulo-02/back-integral-datas-em-js/extra-07/index.js
@@ -0,0 +1,19 @@
+import dayjs from 'dayjs'
+import isBetween from 'dayjs/plugin/isBetween.js'
+
+dayjs.extend(isBetween)
+
+const clientArrived = new Date(2023, 7, 30, 12, 30)
+
+function isPromotionStillAvailable(clientArrived) {
+  const client = dayjs(clientArrived)
+
+  const promotionStarts = dayjs()
+  const promotionEnds = dayjs().add(30, 'day')
+
+  const isPromotionValid = client.isBetween(promotionStarts, promotionEnds)
+
+  return isPromotionValid
+}
+
+console.log(isPromotionStillAvailable(clientArrived))
