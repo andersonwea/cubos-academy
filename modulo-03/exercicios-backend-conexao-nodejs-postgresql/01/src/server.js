42f1d91b880a40da5533a8979b1d3d78a36e388f
chain <klaus.mika1052@gmail.com>
2023-08-21 18:40:41 -0300

    create server

---
+++ modulo-03/exercicios-backend-conexao-nodejs-postgresql/01/src/server.js
@@ -0,0 +1,8 @@
+import { app } from './app.js'
+import 'dotenv/config'
+
+const PORT = process.env.PORT ?? 3333
+
+app.listen(PORT, () => {
+  console.log(`🚀 HTTP server running on port http://localhost:${PORT}`)
+})
