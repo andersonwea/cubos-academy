687fe33a2be23ef21d2f8d97080c140877585f45
chain <klaus.mika1052@gmail.com>
2023-08-21 21:53:09 -0300

    fix port variable

--- modulo-03/exercicios-backend-conexao-nodejs-postgresql/01/src/database.js
+++ modulo-03/exercicios-backend-conexao-nodejs-postgresql/01/src/database.js
@@ -1,9 +1,11 @@
-import { Pool } from 'pg'
+import pg from 'pg'
 import { env } from './env/index.js'
+
+const { Pool } = pg
 
 export const pool = new Pool({
   host: env.PGHOST,
-  port: env.PORT,
+  port: env.PGPORT,
   database: env.PGDATABASE,
   user: env.PGUSER,
   password: env.PGPASSWORD,
