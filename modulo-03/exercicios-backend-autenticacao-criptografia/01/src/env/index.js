e3dd2c685149ae95d22ea990d165477a6303a66a
chain <klaus.mika1052@gmail.com>
2023-08-24 17:13:16 -0300

    adjust env environments variables

--- modulo-03/exercicios-backend-autenticacao-criptografia/01/src/env/index.js
+++ modulo-03/exercicios-backend-autenticacao-criptografia/01/src/env/index.js
@@ -11,6 +11,8 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   PGHOST: z.string(),
   PGPORT: z.coerce.number(),
   PGDATABASE: z.string(),
@@ -18,7 +20,7 @@ const envSchema = z.object({
   PGPASSWORD: z.string(),
 })
 
-const _env = envSchema.parse(process.env)
+const _env = envSchema.safeParse(process.env)
 
 if (_env.sucess === false) {
   console.error('Invalid environment variables.', _env.error.format())
