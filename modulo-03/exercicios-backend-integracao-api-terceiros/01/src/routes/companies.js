90fffebb7aaaa7e5d926a0f3d49c4dd05c0cb533
chain <klaus.mika1052@gmail.com>
2023-08-26 20:55:02 -0300

    feat: create route to get companies

--- modulo-03/exercicios-backend-integracao-api-terceiros/01/src/routes/companies.js
+++ modulo-03/exercicios-backend-integracao-api-terceiros/01/src/routes/companies.js
@@ -1,5 +1,6 @@
 import express from 'express'
+import { getCompany } from '../controllers/company-controller.js'
 
 export const companyRoutes = express()
 
-companyRoutes.get('/companies')
+companyRoutes.get('/companies/:domain', getCompany)
