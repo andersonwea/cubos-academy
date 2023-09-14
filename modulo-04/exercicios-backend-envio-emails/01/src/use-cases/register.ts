c26a5723aaaddddfb537429ec535cd1a07d2ad94
chain <klaus.mika1052@gmail.com>
2023-09-14 20:22:24 -0300

    create register user use-case

--- modulo-04/exercicios-backend-envio-emails/01/src/use-cases/register.ts
+++ modulo-04/exercicios-backend-envio-emails/01/src/use-cases/register.ts
@@ -1,8 +1,33 @@
+import { User } from "@prisma/client"
+import { UsersRepository } from "../repositories/users-repository"
+import { UserAlreadyExists } from "./errors/user-already-exists-error"
+
 interface RegisterUseCaseRequest {
   name: string
   email: string
 }
 
 interface RegisterUseCaseResponse {
-  
+  user: User
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ name, email }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExists()
+    }
+
+    const user = await this.usersRepository.create({
+      name,
+      email
+    })
+
+    return {
+      user
+    }
+  }
 }
