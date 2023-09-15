b715db3cc5ac25e5b9972def4308e8b44e80b127
chain <klaus.mika1052@gmail.com>
2023-09-14 21:12:45 -0300

    create env validate

---
+++ modulo-04/exercicios-backend-envio-emails/01/src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  SMTP_HOST: z.string(),
+  SMTP_PORT: z.number(),
+  SMTP_USER: z.string(),
+  SMTP_PASS: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('⚠️ Invalid environment variables.', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
